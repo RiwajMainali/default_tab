d118262d2699e28b652e3a69c98b048c797816c3
Riwaj Mainali <riwaj227@gmail.com>
2022-08-15 23:49:41 -0500

    basic layout, initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [showSearch, setShowSearch] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <header className="App-header">Hello There</header>
+      <body>
+        <div className="top">
+          <span className="topLeft">Search Engine</span>{" "}
+          <span className="topRight">Top Right</span>
+          <form className="searchForm">
+            <input />
+            <button>Search</button>
+          </form>
+          <form className="youtubeForm">
+            <input />
+            <button>Search</button>
+          </form>
+        </div>
+        <div className="middle">
+          <span className="middleLeft">Middle Left</span>{" "}
+          <span className="middleRight">Middle Right</span>
+        </div>
+
+        <div className="bottom">
+          <span className="bottomLeft">Bottom Left</span>{" "}
+          <span className="bottomRight">Bottom Right</span>
+        </div>
+      </body>
     </div>
   );
 }
---
+++ src/components/Search.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+interface Props {
+  Search: string;
+  Tags?: string[];
+}
+export const Search = (Props: Props) => {
+  return <div>Search</div>;
+};
